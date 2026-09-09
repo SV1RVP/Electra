47bc00c11e3ee23023bdcd2c50e3466ee182beff
SV1RVP <sv1rvp@users.noreply.github.com>
2026-09-09 12:38:06 +0300

    Release Electra v1.4.1

--- updater.py
+++ updater.py
@@ -62,7 +62,7 @@ def get_local_version() -> Dict[str, Any]:
         "app_name": "Electra",
         "name": "Electra - UPS Status Central Monitor",
         "subtitle": "UPS Status Central Monitor",
-        "version": "1.4.0",
+        "version": "1.4.1",
         "author": "Alexandros - Ermis Tsourapas (SV1RVP)",
         "license": "GNU Affero General Public License v3.0 (AGPL-3.0)"
     }
